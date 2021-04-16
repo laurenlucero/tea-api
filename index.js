116c7d34f460e015cdde96d1d23bd2ee5fb10a88
“Lauren <lucerolaurenm@gmail.com>
2021-04-16 13:24:46 -0400

    Add html and css

---
+++ index.js
@@ -0,0 +1,21 @@
+// display a <div></div> element with the name of the menu item clicked on
+
+// getAll
+// use getJSON to fetch data from our deployed API with the route /tea
+// After the data is fetched, a callback fn will parse and format the JSON and append it to the display <div> as a <pre> (preformatted) element
+
+/* getOne */
+//append input elements upon menu item being clicked
+
+// if the submit button is clicked, fetch data with the specified name
+
+//display the fetched data in a nice format
+
+/* postOne */
+//append input elements upon menu item being clicked
+
+// if submit comment btn is clicked, post the comment data
+//request method
+//data to post
+
+// if post succeeds, display the comments
